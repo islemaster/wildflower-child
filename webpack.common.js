234ddf67a3eed05ff5c70f6e50da51950ef191f3
Brad Buchanan <bradley.c.buchanan@gmail.com>
2017-11-30 01:10:35 -0800

    Create a release build

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,17 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 
   entry: './src/index.js',
 
+  plugins: [
+    new CleanWebpackPlugin(['dist/bundle.js']),
+  ],
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
   },
 
   module: {
